267dabc831f5bfc2620aff36d4ce8976fb7e68ab
SzukicsA <118008435+SzukicsA@users.noreply.github.com>
2025-05-05 15:16:06 +0200

    Add files via upload

--- Bluetooth.rs
+++ Bluetooth.rs
@@ -1,1 +1,11 @@
-Learn more about New+ by visiting https://aka.ms/PowerToysOverview_NewPlus
+$ cargo new foo
+    Creating binary (application) `foo` package
+note: see more `Cargo.toml` keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+$ cat foo/Cargo.toml
+[package]
+name = "foo"
+version = "0.1.0"
+edition = "2024"
+
+[dependencies]
+
