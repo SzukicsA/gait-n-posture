266a3cbb034903f371c937d8984fff6fcebb52a5
Szukics Ortiz <andreas.szukics@unibe.ch>
2025-05-06 17:45:05 +0200

    new code in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, bluetooth!");
 }
+
+use tokio::main;            //load the package for bluetooth 
+
+async main() {              //call the function to open the 
+    println!("tokio loaded")
+}
+
+use btleplug::platform      //load bluetooth manager to connect with devices
+Manager::new() {            //call the function to open the 
+    println!("Manager loaded")
+}
+
