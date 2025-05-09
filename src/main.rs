ec2278d79f21727a7b8b7293b9511e34bb15f3b8
Szukics Ortiz <andreas.szukics@unibe.ch>
2025-05-09 14:28:52 +0200

    modified main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use btleplug::platform::{Adapter, Manager};                        // for Bluetooth manager
-use tokio::time::{sleep, Duration};                     // for pause/wait
+use btleplug::platform::{Adapter, Manager};                         // for Bluetooth manager
+use tokio::time::{sleep, Duration};                                 // for pause/wait
 
 fn start() {
     println!("Hello, tester!");
 }
 
 #[tokio::main] 
-async fn main() {                                                                                                        // call the function to open tokio 
-    let manager = match Manager::new().await {                                                                   // the function with a check if it loads?
-        Ok(m) => {                                      // Message whether it loaded? 
+async fn main() {                                                   // call the function to open tokio 
+    let manager = match Manager::new().await {                      // the function with a check if it loads?
+        Ok(m) => {                                                  // Message whether it loaded? 
             println!("Manager loaded");                            
             m
         },
         Err(e) => {
-            eprintln!("Failed loading manager: {:?}",  // Message whether it didn't load?
+            eprintln!("Failed loading manager: {:?}",               // Message whether it didn't load?
 
             e);
             return;
